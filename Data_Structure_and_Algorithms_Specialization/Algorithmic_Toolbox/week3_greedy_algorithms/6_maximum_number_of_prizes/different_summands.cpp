89c2b96167343d425546b92d12652771d7b8df7d
naira <yeghiazaryan.naira1@gmail.com>
2022-05-23 17:45:00 +0400

    feat(week3): solve maximum number of prizes assignment

---
+++ Data_Structure_and_Algorithms_Specialization/Algorithmic_Toolbox/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+vector<int> optimal_summands(int n) {
+  vector<int> summands;
+  if (n == 0) {
+  summands.push_back(0);
+    return summands;
+  };
+  
+  int sum = 1;
+  summands.push_back(1);
+  int next;
+
+
+  
+  while (sum < n) {
+    int last = summands[summands.size() - 1];
+    next = last + 1;
+    summands.push_back(next);
+    sum += next;
+
+    if (sum + (summands[summands.size() - 1] + 1) > n) {
+
+      sum -= next;
+      next = n - sum;
+      summands[summands.size()-1] = next;
+      sum += next;
+      break;
+ 
+      }
+  };
+  return summands;
+}
+
+int main() {
+  int n;
+  std::cin >> n;
+  vector<int> summands = optimal_summands(n);
+  std::cout << summands.size() << '\n';
+  for (size_t i = 0; i < summands.size(); ++i) {
+    std::cout << summands[i] << ' ';
+  }
+}
