b37fb245ce7e00a5fb59f824fadbd8defa98bbb2
naira <yeghiazaryan.naira1@gmail.com>
2022-05-23 18:38:37 +0400

    feat(week3): solve maximum salery asignments

---
+++ Data_Structure_and_Algorithms_Specialization/Algorithmic_Toolbox/week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+
+using std::vector;
+using std::string;
+
+bool myCmp(string s1, string s2)
+{
+    if ((s1+s2).compare((s2+s1)) < 0) {
+        return s1 < s2;
+    };
+
+    if ((s1+s2).compare((s2+s1)) > 0) {
+        return s1 > s2;
+    };
+    
+    return 0;
+}
+
+
+string largest_number(vector<string> a) {
+
+  sort(a.begin(), a.end(), myCmp);
+
+  std::stringstream ret;
+  for (size_t i = 0; i < a.size(); i++) {
+    ret << a[i];
+  }
+  string result;
+  ret >> result;
+  return result;
+};
+
+int main() {
+  int n;
+  std::cin >> n;
+  vector<string> a(n);
+  for (size_t i = 0; i < a.size(); i++) {
+    std::cin >> a[i];
+  }
+  std::cout << largest_number(a);
+}
