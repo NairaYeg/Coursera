77c682fa3182cb462ecfd9112fbe071d4ba50405
naira <yeghiazaryan.naira1@gmail.com>
2022-06-08 18:17:33 +0400

    fix(week5): bug in edit_distance function

--- Data_Structure_and_Algorithms_Specialization/Algorithmic_Toolbox/week5_dynamic_programming1/3_edit_distance/edit_distance.cpp
+++ Data_Structure_and_Algorithms_Specialization/Algorithmic_Toolbox/week5_dynamic_programming1/3_edit_distance/edit_distance.cpp
@@ -1,47 +1,49 @@
-
 #include <iostream>
-#include <algorithm>
 #include <string>
-#include <vector>
-using std::string;
+#include <algorithm>
+
 using namespace std;
 
+using std::string;
+int getMinValue(int x,int y,int z){
+    if(min(x,y)>z)
+        return z;
+    else 
+        return min(x,y);
+};
+
 int edit_distance(const string &str1, const string &str2) {
-  int m = str1.size();
-  int n = str2.size();
-	vector<vector<int> > tmp(m+1, std::vector<int>(n+1));
 
-	for(int i = 0; i<= m; i++) {
-		for(int j = 0; j<= n; j++){
-
-			if( i == 0 ) {   
-				tmp[i][j] = j;
-      } else if(j == 0) {    
-				tmp[i][j] = i;
-      } else if(str1[i] == str2[j]) {
-        
-        int insertion  = tmp[i][j-1] + 1;
-        int deletion = tmp[i-1][j] + 1;
-        int match = tmp[j-1][i-1];
-        
-        tmp[i][j] = min(insertion, deletion, match);
-        
-      } else {
-        int insertion  = tmp[i][j-1] + 1;
-        int deletion = tmp[i-1][j] + 1;
-        int mismatch = tmp[i-1][j-1] + 1;
-        
-        tmp[i][j] = min(mismatch, deletion, insertion);
-			};
-	};
-};
-	return tmp[m][n];
-};
+  int n = str1.size(),m=str2.size();
+  int tmp[n+1][m+1];
+  int insertion,mismatch,match,deletion;
+  
+  for(int i=0;i<n+1;i++)
+    tmp[i][0]=i;
+  
+  for(int j=0;j<m+1;j++)
+    tmp[0][j]=j;
+    
+  for(int i=1;i<n+1;i++){
+      for(int j=1;j<m+1;j++){
+            insertion =tmp[i][j-1]+1;
+            deletion = tmp[i-1][j]+1;
+            match = tmp[i-1][j-1];
+            mismatch = tmp[i-1][j-1]+1;
+          if(str1[i-1]==str2[j-1])
+            tmp[i][j]=getMinValue(insertion,deletion,match);
+        else
+            tmp[i][j]= getMinValue(insertion,deletion,mismatch);
+      }
+  }
+  
+  return tmp[n][m];
+}
 
 int main() {
   string str1;
   string str2;
   std::cin >> str1 >> str2;
-  std::cout << edit_distance(str1, str2)  << std::endl;
+  std::cout << edit_distance(str1, str2) << std::endl;
   return 0;
 }
